427e53ed836d16d69443a2a4cdc43f2500ca09ac
Peter Piper Rosendahl <peter.piper.rosendahl@gmail.com>
2014-03-13 20:38:40 +0100

    styling

--- Gemfile
+++ Gemfile
@@ -10,7 +10,7 @@ gem 'devise', '~> 3.2.2'
 
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.0'
-gem 'foundation-rails', '~> 5.0.3.1'
+gem 'foundation-rails', '~> 5.2.0.0'
 gem 'foundation_rails_helper'
 
 # Use Uglifier as compressor for JavaScript assets
--- app/assets/javascripts/show-mode-tab.js
+++ app/assets/javascripts/show-mode-tab.js
@@ -1,14 +1,14 @@
 $(document).on('click', '#map-mode-tab', function () {
-    $(this).addClass('active-mode-tab')
-    $('#list-mode-tab').removeClass('active-mode-tab')
+    $(this).removeClass('inactive-tab');
+    $('#list-mode-tab').addClass('inactive-tab');
 
     $('#list-container').addClass('hide-for-small');
     $('#map-container').removeClass('hide-for-small');
 });
 
 $(document).on('click', '#list-mode-tab', function () {
-    $(this).addClass('active-mode-tab')
-    $('#map-mode-tab').removeClass('active-mode-tab')
+    $(this).removeClass('inactive-tab');
+    $('#map-mode-tab').addClass('inactive-tab');
 
     $('#map-container').addClass('hide-for-small');
     $('#list-container').removeClass('hide-for-small');
