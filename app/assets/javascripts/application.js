8ed36379610d937e8a13ae7e37488ba4873635ff
Peter Piper Rosendahl <peter.piper.rosendahl@gmail.com>
2014-03-14 13:54:59 +0100

    messages now disappear after 5 seconds

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,10 +13,33 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require foundation
+//= require foundation/foundation
+//= require foundation/foundation.abide
+//= require foundation/foundation.accordion
+//= require foundation/foundation.alert
+//= require foundation/foundation.clearing
+//= require foundation/foundation.dropdown
+//= require foundation/foundation.interchange
+//= require foundation/foundation.joyride
+//= require foundation/foundation.magellan
+//= require foundation/foundation.offcanvas
+//= require foundation/foundation.orbit
+//= require foundation/foundation.reveal
+//= require foundation/foundation.tab
+//= require foundation/foundation.tooltip
+//= require foundation/foundation.topbar
+//= require foundation/foundation.equalizer
 //= require_tree .
+//= require_self
+
 function ready() {
     $(document).foundation();
+
+    if ($('.alert-box').length >= 0) {
+        setTimeout(function () {
+            $('.alert-box .close').click();
+        }, 5000);
+    }
 }
 
 $(document).ready(ready)
