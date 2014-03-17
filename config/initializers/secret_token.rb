f30820db062eb4953c0a1b8a2a2425ded9fa7b52
Peter Piper Rosendahl <peter.piper.rosendahl@gmail.com>
2014-03-17 16:55:00 +0100

    Secret token added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Uvhunter::Application.config.secret_key_base = 'dd392bb24ed51b70f69a16ea4779c3436fb16e2307c8c7e2bba0244ce66bb4c1113dc4ae08dc64325db692b5415b94534bb2f101b6978105e1e8bb40f30da006'
