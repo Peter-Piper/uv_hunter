65aac45600bc9d2e96345a0efbba6d9cba4ce697
Peter Piper Rosendahl <peter.piper.rosendahl@gmail.com>
2014-03-28 15:09:35 +0100

    Issue with fieldset with current password being hidden if current password was wrong is now fixed

--- app/assets/javascripts/devise.js
+++ app/assets/javascripts/devise.js
@@ -2,7 +2,6 @@ $(document).on('click', '#update-btn', function(){
   $('#edit-box').addClass('hide');
   $('#current-pwd-box').removeClass('hide');
 });
-
 $(document).on('click', '#current-pwd-box-closer', function(){
     $('#current-pwd-box').addClass('hide');
     $('#edit-box').removeClass('hide');
