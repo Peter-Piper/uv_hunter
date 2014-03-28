1b209563619c804d599d3ceffb8618f0df949232
Peter Piper Rosendahl <peter.piper.rosendahl@gmail.com>
2014-03-28 14:40:25 +0100

    Devise forms cleaned up and translated. General I18n is setup

---
+++ app/assets/javascripts/devise.js
@@ -0,0 +1,9 @@
+$(document).on('click', '#update-btn', function(){
+  $('#edit-box').addClass('hide');
+  $('#current-pwd-box').removeClass('hide');
+});
+
+$(document).on('click', '#current-pwd-box-closer', function(){
+    $('#current-pwd-box').addClass('hide');
+    $('#edit-box').removeClass('hide');
+});
